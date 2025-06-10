3a1a1afb1c26d5176df7501f31c1388b04be661a
Millie <milliedarc@gmail.com>
2025-06-10 15:42:04 +0100

    First working version

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,146 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {ref, computed} from "vue";
 
-const divArray = ref(['', '', '', '', '', '', '', '', ''])
+const board = ref<string[]>(['', '', '', '', '', '', '', '', ''])
+const currentPlayer = ref<'playerOne' | 'playerTwo'>('playerOne');
 
-function changeDivColour(i: number) {
-  if (divArray.value[i] === '') {
-    divArray.value[i] = 'background-color: #D86E6EFF;'
-  } else {
-    divArray.value[i] = '';
+interface Player {
+  colour: string;
+  display: string
+}
+
+const players: Record<'playerOne' | 'playerTwo', Player> = {
+  playerOne: {
+    colour: 'background-color: #415e83;',
+    display: 'Player 1'
+  },
+  playerTwo: {
+    colour: 'background-color: #7aa5dc;',
+    display: 'Player 2'
   }
+}
+
+const winner = computed((): 'playerOne' | 'playerTwo' | 'draw' | null => {
+  const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+
+  for (const pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (board.value[a] && board.value[a] === board.value[b] && board.value[a] === board.value[c]) {
+      return board.value[a] as 'playerOne' | 'playerTwo';
+    }
+  }
+  if (board.value.every(tile => tile !== '')) {
+    return 'draw';
+  }
+  return null;
+})
+
+function makeMove(i: number): void {
+  if (board.value[i] === '' && !winner.value) {
+    board.value[i] = currentPlayer.value;
+    currentPlayer.value = currentPlayer.value === 'playerOne' ? 'playerTwo' : 'playerOne';
+  }
+}
+
+function resetGame(): void {
+  board.value = ['', '', '', '', '', '', '', '', ''];
+  currentPlayer.value = 'playerOne';
 }
 
 </script>
 
 <template>
-  <div>
-    <div class="container">
-      <div v-for="i in [0, 1, 2]" @click="changeDivColour(i)" :style="divArray[i]">{{ i + 1 }}</div>
+  <section class="text-section">
+    <h4>Click on the tiles to play.</h4>
+    <div v-if="!winner">
+      <h5 class="current-player"
+          :style="players[currentPlayer].colour">Current player: {{ players[currentPlayer].display }}</h5>
     </div>
-    <div class="container">
-      <div v-for="i in [3, 4, 5]" @click="changeDivColour(i)" :style="divArray[i]">{{ i + 1 }}</div>
+    <div v-else-if="winner === 'draw'">
+      <h5>It's a draw!</h5>
     </div>
-    <div class="container">
-      <div v-for="i in [6, 7, 8]" @click="changeDivColour(i)" :style="divArray[i]">{{ i + 1 }}</div>
+    <div v-else>
+    <span :style="players[winner].colour">
+      {{ players[winner].display }} has won!
+    </span>
     </div>
-  </div>
+  </section>
+  <section class="board">
+    <div class="grid">
+      <div class="tile"
+           v-for="i in [0, 1, 2, 3, 4, 5, 6, 7, 8]"
+           @click="makeMove(i)"
+           :style="board[i] ? players[board[i] as 'playerOne' | 'playerTwo'].colour : ''">
+        {{ i + 1 }}
+      </div>
+    </div>
+  </section>
+  <section class="button-section">
+    <button @click="resetGame">Reset game</button>
+  </section>
 </template>
 
 <style scoped>
-
-.container {
-  display: flex;
-  justify-content: center;
+* {
+  font-family: 'Nunito', sans-serif;
 }
 
-.container div {
+.current-player {
+  padding: 4px 8px;
+  border-radius: 4px;
+  color: #ffffff;
+}
+
+.text-section,
+.board {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  border: 2px solid black;
+}
+
+.tile {
   padding: 50px;
-  border: #000000 1px solid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-right: 2px solid black;
+  border-bottom: 2px solid black;
+}
+
+.tile:nth-child(3n) {
+  border-right: none;
+}
+
+.tile:nth-last-child(-n+3) {
+  border-bottom: none;
+}
+
+.button-section {
+  display: flex;
+  justify-content: center;
+  padding: 50px;
+}
+
+button {
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: #353232;
+  color: whitesmoke;
+}
+
+button:hover {
+  background-color: white;
+  color: black;
 }
 </style>
